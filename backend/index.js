57a70f7f3e57c840028bb27ccd08f6b8d79aa41d
RavjotSandhu <nand2072@gmail.com>
2021-09-19 19:45:13 +0530

    some changes

--- backend/index.js
+++ backend/index.js
@@ -5,7 +5,7 @@ var commRoutes = require("./routes/communities");
 // var profileRoutes = require("./routes/profile");
 var cors = require("cors");
 app.use(cors());
-require("./db/mysql");
+require("./routes/communities");
 
 // Access body request of posts
 app.use(express.json());
@@ -14,8 +14,28 @@ app.get("/", function (req, res) {
     res.send("loaded");
 });
 app.use("/communities", commRoutes);
+app.use("/communities/addcommunity", commRoutes);
+app.use("/HealthGroup", commRoutes);
+app.use("/SportsGroup", commRoutes);
 // app.use("/profile", profileRoutes);
 
 app.listen(3001, function () {
     console.log("App run");
 });
+// const express = require('express')
+// const bodyParser = require('body-parser')
+// const app = express()
+// const port = 3000
+
+// app.use(bodyParser.json())
+// app.use(
+//   bodyParser.urlencoded({
+//     extended: true,
+//   })
+// )
+// app.get('/', (request, response) => {
+//     response.json({ info: 'Node.js, Express, and Postgres API' })
+//   })
+//   app.listen(port, () => {
+//     console.log(`App running on port ${port}.`)
+//   })
--- backend/routes/communities.js
+++ backend/routes/communities.js
@@ -1,50 +1,86 @@
 var express = require("express");
+var app = express();
 var router = express.Router();
 require("dotenv").config();
 // Import mysql for node
-var connection = require("../db/mysql");
+
+var mysql      = require('mysql');
+var connection = mysql.createConnection({
+  host     : 'localhost',
+  user     : 'root',
+  password : '',
+});
+connection.connect(function(err) {
+    if (err) {
+      console.error('error connecting: ' + err.stack);
+      return;
+    }
+    console.log('connected as id ' + connection.threadId);
+  });
 
 router.get("/", async function (req, res) {
     /* Getting all the communites */ 
     // let sql = "CREATE DATABASE nodemysql";
-    // connection.query(sql, (err) =>{
-    //     if(err){
-    //         throw err;
-    //     }
-    //     res.send("Database created");
-    try {
-        const conn = await connection.find({});
-        res.send(conn);
-    } catch (e) {
-        res.status(500).send();
-    }
+    connection.query("SELECT * FROM information.communitites", (err,rows,fields) =>{
+        if(!!err){
+            console.log('Error int the query');
+        }
+        else{
+            console.log('Success!\n');
+            console.log(rows);
+            // res.json(rows);
+        }
     });
+});
 
-// router.get("/:name", async function (req, res) {
-//     // Get specific community name
-//     try {
-//         const milk = await Milk.findOne(
-//             { name: req.params.name },
-//             async (err, data) => {
-//                 if (!data) return;
-//                 return data;
-//             }
-//         );
-//         res.send(milk);
-//     } catch (e) {
-//         res.status(500).send();
-//     }
-// });
+router.get("/:name", async function (req, res) {
+    // Get specific community name
+    // let name = req.params.name;
+    // let sql = "SELECT * FROM communitites WHERE name = '" + name + "'";
+    connection.query("SELECT * FROM information.communitites WHERE name=?",req.params.name,(err,rows,fields) =>{
+        if(!!err){
+            console.log('Error int the query');
+        }
+        else{
+            console.log('Success!\n');
+            // console.log(rows.name);
+        }
+    });
+    // try {
+    //     const milk = await Milk.findOne(
+    //         { name: req.params.name },
+    //         async (err, data) => {
+    //             if (!data) return;
+    //             return data;
+    //         }
+    //     );
+    //     res.send(milk);
+    // } catch (e) {
+    //     res.status(500).send();
+    // }
+});
 
-// router.post("/", async function (req, res) {
-//     // Post a new community onto the sql database
-//     const milk = new Milk(req.body);
-//     try {
-//         await milk.save();
-//         res.status(201).send({ milk });
-//     } catch (e) {
-//         res.status(400).send(e);
-//     }
-// });
+router.post("/:addcommunity", async function (req, res) {
+    //id,fullname,money
+    let sql = "INSERT INTO information.communitites (id, name, money) VALUES (3, 'SchoolGroup', 100000)";
+    connection.query(sql, (err,result) =>{
+        if(!!err){
+            console.log('Error int the query');
+        }
+        else{
+            console.log('Success!\n');
+            console.log(result);
+            // res.json(rows);
+        }
+    });
+    // Post a new community onto the sql database
+    // const milk = new Milk(req.body);
+    // try {
+    //     await milk.save();
+    //     res.status(201).send({ milk });
+    // } catch (e) {
+    //     res.status(400).send(e);
+    // }
+});
 
 module.exports = router;
