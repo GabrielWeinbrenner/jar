0156a1a624a7cb324e1a469f846647ef7f7c3280
RavjotSandhu <nand2072@gmail.com>
2021-09-19 16:12:37 +0530

    connected database

--- backend/index.js
+++ backend/index.js
@@ -1,11 +1,11 @@
 var express = require("express"),
     app = express(),
     bodyParser = require("body-parser");
-var communitiesroutes = require("./routes/communities");
-var profileRoutes = require("./routes/profile");
+var commRoutes = require("./routes/communities");
+// var profileRoutes = require("./routes/profile");
 var cors = require("cors");
 app.use(cors());
-require("./db/mongoose");
+require("./db/mysql");
 
 // Access body request of posts
 app.use(express.json());
@@ -13,9 +13,8 @@ app.use(express.urlencoded({ extended: true }));
 app.get("/", function (req, res) {
     res.send("loaded");
 });
-app.use("/milk", communitiesroutes);
-
-app.use("/profile", profileRoutes);
+app.use("/communities", commRoutes);
+// app.use("/profile", profileRoutes);
 
 app.listen(3001, function () {
     console.log("App run");
--- backend/routes/communities.js
+++ backend/routes/communities.js
@@ -2,40 +2,49 @@ var express = require("express");
 var router = express.Router();
 require("dotenv").config();
 // Import mysql for node
-router.get("/", async function (req, res) {
-    /* Getting all the communites */
-    try {
-        const milks = await Milk.find({});
-        res.send(milks);
-    } catch (e) {
-        res.status(500).send();
-    }
-});
-router.get("/:name", async function (req, res) {
-    // Get specific community name
-    try {
-        const milk = await Milk.findOne(
-            { name: req.params.name },
-            async (err, data) => {
-                if (!data) return;
-                return data;
-            }
-        );
-        res.send(milk);
-    } catch (e) {
-        res.status(500).send();
-    }
-});
+var connection = require("../db/mysql");
 
-router.post("/", async function (req, res) {
-    // Post a new community onto the sql database
-    const milk = new Milk(req.body);
+router.get("/", async function (req, res) {
+    /* Getting all the communites */ 
+    // let sql = "CREATE DATABASE nodemysql";
+    // connection.query(sql, (err) =>{
+    //     if(err){
+    //         throw err;
+    //     }
+    //     res.send("Database created");
     try {
-        await milk.save();
-        res.status(201).send({ milk });
+        const conn = await connection.find({});
+        res.send(conn);
     } catch (e) {
-        res.status(400).send(e);
+        res.status(500).send();
     }
-});
+    });
+
+// router.get("/:name", async function (req, res) {
+//     // Get specific community name
+//     try {
+//         const milk = await Milk.findOne(
+//             { name: req.params.name },
+//             async (err, data) => {
+//                 if (!data) return;
+//                 return data;
+//             }
+//         );
+//         res.send(milk);
+//     } catch (e) {
+//         res.status(500).send();
+//     }
+// });
+
+// router.post("/", async function (req, res) {
+//     // Post a new community onto the sql database
+//     const milk = new Milk(req.body);
+//     try {
+//         await milk.save();
+//         res.status(201).send({ milk });
+//     } catch (e) {
+//         res.status(400).send(e);
+//     }
+// });
 
 module.exports = router;
